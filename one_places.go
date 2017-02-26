739159372f3afeb9e14857eadd6aae27df68f7ab
Emperor Palpatin <ep@kpsz.cz>
2017-02-26 20:30:39 +0100

    one_places.go refactored to sanity

--- one_places.go
+++ one_places.go
@@ -1,32 +1,28 @@
 package fsp
 
 // engine that tries to find at least one solution,
-// not considering time constraints
+// using DFS, not considering time constraints
 type One_places struct{}
 
 func (e One_places) Solve(done <-chan struct{}, p Problem) <-chan Solution {
 	result := make(chan Solution)
 	go func() {
-		result <- solveTODO2(p)
+		stops := stops(p)
+		flights := p.flights
+		if len(stops) < 2 {
+			result <- Solution{}
+			return
+		}
+		// stops = { brq, lon, xxx }
+		// visited = { brq }
+		visited := make([]City, 1, len(stops))
+		visited[0] = stops[0]
+		// to_visit = { lon, xxx, brq }
+		to_visit := append(stops[1:], stops[0])
+		partial := make([]Flight, 0, len(stops))
+		result <- NewSolution(one_dfs(partial, visited, to_visit, flights))
 	}()
 	return result
-}
-
-func solveTODO2(p Problem) Solution {
-	stops := stops(p)
-	flights := p.flights
-	if len(stops) < 2 {
-		return Solution{}
-	}
-	// stops = { brq, lon, xxx }
-	// visited = { brq }
-	visited := make([]City, 1, len(stops))
-	visited[0] = stops[0]
-	// to_visit = { lon, xxx, brq }
-	to_visit := append(stops[1:], stops[0])
-	partial := make([]Flight, 0, len(stops))
-	solution := solveTODO(partial, visited, to_visit, flights)
-	return NewSolution(solution)
 }
 
 func indexOf(haystack []City, needle City) int {
@@ -38,14 +34,14 @@ func indexOf(haystack []City, needle City) int {
 	return -1
 }
 
-func solveTODO(partial []Flight, visited, to_visit []City, flights []Flight) []Flight {
+func one_dfs(partial []Flight, visited, to_visit []City, flights []Flight) []Flight {
 	if len(to_visit) == 0 {
 		return partial
 	}
 	for _, f := range flights {
 		if f.from == visited[len(visited)-1] {
 			if si := indexOf(to_visit, f.to); si != -1 {
-				solution := solveTODO(append(partial, f),
+				solution := one_dfs(append(partial, f),
 					append(visited, f.to),
 					append(to_visit[:si], to_visit[si+1:]...),
 					flights)
