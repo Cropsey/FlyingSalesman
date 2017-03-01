1593e63b44d84fecbed6023cc9473bd016f20ae7
wozy <wozniak.jan@gmail.com>
2017-02-28 21:40:59 -0500

    go fmt

--- cmd/main.go
+++ cmd/main.go
@@ -4,11 +4,11 @@ import (
 	"bufio"
 	"fmt"
 	"fsp"
+	"github.com/pkg/profile"
 	"os"
+	"runtime/debug"
 	"strconv"
 	"strings"
-	"github.com/pkg/profile"
-	"runtime/debug"
 )
 
 /*var engines = []fsp.Engine {
@@ -16,36 +16,36 @@ import (
 }*/
 
 type lookup struct {
-    cityToIndex map[string]uint32
-    indexToCity []string
+	cityToIndex map[string]uint32
+	indexToCity []string
 }
 
 func getIndex(city string, l *lookup) uint32 {
-    ci, found := l.cityToIndex[city]
-    if found {
-        return ci
-    }
-    ci = uint32(len(l.cityToIndex))
-    l.cityToIndex[city] = ci
-    l.indexToCity = append(l.indexToCity, city)
-    return ci
+	ci, found := l.cityToIndex[city]
+	if found {
+		return ci
+	}
+	ci = uint32(len(l.cityToIndex))
+	l.cityToIndex[city] = ci
+	l.indexToCity = append(l.indexToCity, city)
+	return ci
 }
 func readInput() fsp.Problem {
-    lookup := &lookup{make(map[string]uint32), make([]string, 0, fsp.MAX_CITIES)}
+	lookup := &lookup{make(map[string]uint32), make([]string, 0, fsp.MAX_CITIES)}
 	flights := make([]fsp.Flight, 0, fsp.MAX_FLIGHTS)
 
 	var src string
 	stdin := bufio.NewScanner(os.Stdin)
 	if stdin.Scan() {
 		src = stdin.Text()
-        getIndex(src, lookup)
+		getIndex(src, lookup)
 	}
 	for stdin.Scan() {
 		l := customSplit(stdin.Text())
 		day, _ := strconv.Atoi(l[2])
 		cost, _ := strconv.Atoi(l[3])
-        from := getIndex(l[0], lookup)
-        to := getIndex(l[1], lookup)
+		from := getIndex(l[0], lookup)
+		to := getIndex(l[1], lookup)
 		flight := fsp.NewFlight(from, to, uint16(day), cost)
 		flights = append(flights, flight)
 	}
@@ -54,16 +54,16 @@ func readInput() fsp.Problem {
 }
 
 func customSplit(s string) []string {
-        /* Splits lines of input into 4 parts
-           strictly expects format "{3}[A-Z] {3}[A-Z] \d \d"
-           WARNING: no checks are done at all */
-        r := make([]string, 4)
-        r[0] = s[:3]
-        r[1] = s[4:7]
-        pos2 := strings.LastIndexByte(s, ' ')
-        r[2] = s[8:pos2]
-        r[3] = s[pos2+1:]
-        return r
+	/* Splits lines of input into 4 parts
+	   strictly expects format "{3}[A-Z] {3}[A-Z] \d \d"
+	   WARNING: no checks are done at all */
+	r := make([]string, 4)
+	r[0] = s[:3]
+	r[1] = s[4:7]
+	pos2 := strings.LastIndexByte(s, ' ')
+	r[2] = s[8:pos2]
+	r[3] = s[pos2+1:]
+	return r
 }
 
 func kickTheEngines(graph fsp.Graph) fsp.Solution {
