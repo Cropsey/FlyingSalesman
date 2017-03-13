10c9d1f7e9169d2937e0738c621e5167ec3a0aeb
Jan Richter <dzejar@dzejar.net>
2017-03-13 14:39:20 +0100

    removed test dependency on `bc` (hope `awk` is present on travis)

--- perf_tests.bash
+++ perf_tests.bash
@@ -115,6 +115,6 @@ printf "%78s\n" | tr ' ' -
 reference_improvement=$(( $reference_total - $sum))
 best_improvement=$(( $best_reference_total - $sum))
 printf "%20s %7d %31s %8d %8d\n" "Total:" $sum "Improvement:" $reference_improvement $best_improvement
-printf "%61s (%4.1f%%) (%4.1f%%)" " "   $( bc <<< "scale=1; $reference_improvement / ($reference_total / 100)" ) \
-					$( bc <<< "scale=1; $best_improvement / ($best_reference_total / 100)" )
+printf "%61s (%4.1f%%) (%4.1f%%)" " "   $( echo - | awk "{ print $reference_improvement / $reference_total * 100 }" ) \
+					$( echo - | awk "{ print $best_improvement / $best_reference_total * 100 }" )
 exit $RETVAL
