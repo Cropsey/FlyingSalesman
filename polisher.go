34bf1b38db8881d0b0957031e0ebf2b1ddb903be
wozy <wozniak.jan@gmail.com>
2017-03-18 18:22:57 -0400

    3-opt polisher

--- polisher.go
+++ polisher.go
@@ -2,11 +2,12 @@ package fsp
 
 import (
 	"time"
+    "math/rand"
 )
 
 type Polisher struct {
-	graph  Graph
-	update chan update
+	graph   Graph
+	update  chan update
 }
 
 func (p Polisher) Name() string {
@@ -29,7 +30,8 @@ func (p Polisher) try(u update) {
 
 func (p Polisher) Solve(comm comm, problem Problem) {
 	for u := range p.update {
-		go p.run(comm, u, time.After(3*time.Second))
+		go p.run2(comm, u)
+		go p.run3(comm, u)
 	}
 }
 
@@ -43,11 +45,10 @@ A      B      C      D      A
 a->b   b->c   c->d   d->a
 fiPrev fi     fjPrev fj
 
-swap 1 and 3
 a->d   d->c   c->b   b->a
 giPrev gi     gjPrev gj
 */
-func swap(comm comm, g Graph, u update, i, j int) bool {
+func swap(comm comm, g Graph, u update, i, j int) {
 	flights := u.solution.flights
 	prevI := i - 1
 	prevJ := j - 1
@@ -78,34 +79,39 @@ func swap(comm comm, g Graph, u update, i, j int) bool {
 				swapped[x] = flights[x]
 			}
 			comm.send(NewSolution(swapped), u.originalEngine)
-			return true
 		}
 	}
-	return false
 }
 
 /*
-0 ---- 1 ---- 2 ---- 3
-A      B      C      A
-a->b   b->c   c->a
-fiPrev fi     fj
+0 ---- 1 ---- 2 ---- 3 ---- 4 ---- 5 ---- 6
+A      B      C      D      E      F      G
+a->b   b->c   c->d   d->e   e->f   f->g   g->a
+fiPrev fi     fjPrev fj     fkPrev fk
 
-swap 1 and 2
-a->c   c->b   b->a
-giPrev gi     gj
+a->d   d->c   c->f   f->e   e->b   b->g   g->a
+giPrev gi     gjPrev gj     gkPrev gk
 */
-func swapAdj(comm comm, g Graph, u update, i, j int) {
+func swap3a(comm comm, g Graph, u update, i, j, k int) {
 	flights := u.solution.flights
 	prevI := i - 1
+	prevJ := j - 1
+	prevK := k - 1
 	fiPrev := flights[prevI]
+	fjPrev := flights[prevJ]
+	fkPrev := flights[prevK]
+	giPrev := g.get(fiPrev.From, fiPrev.Day, fjPrev.To)
+	gjPrev := g.get(fjPrev.From, fjPrev.Day, fkPrev.To)
+	gkPrev := g.get(fkPrev.From, fkPrev.Day, fiPrev.To)
 	fi := flights[i]
 	fj := flights[j]
-	giPrev := g.get(fiPrev.From, fiPrev.Day, fi.To)
-	gi := g.get(fj.From, fi.Day, fiPrev.To)
-	gj := g.get(fi.From, fj.Day, fj.To)
-	if exists(giPrev) && exists(gi) && exists(gj) {
-		oldF := []Flight{fiPrev, fi, fj}
-		newF := []Flight{*giPrev, *gi, *gj}
+	fk := flights[k]
+	gi := g.get(fj.From, fi.Day, fi.To)
+	gj := g.get(fk.From, fj.Day, fj.To)
+	gk := g.get(fi.From, fk.Day, fk.To)
+	if exists(giPrev) && exists(gjPrev) && exists(gkPrev) && exists(gi) && exists(gj) && exists(gk) {
+		oldF := []Flight{fiPrev, fi, fjPrev, fj, fkPrev, fk}
+		newF := []Flight{*giPrev, *gi, *gjPrev, *gj, *gkPrev, *gk}
 		if Cost(oldF) > Cost(newF) {
 			swapped := make([]Flight, len(flights))
 			for x := 0; x < prevI; x++ {
@@ -113,8 +119,61 @@ func swapAdj(comm comm, g Graph, u update, i, j int) {
 			}
 			swapped[prevI] = *giPrev
 			swapped[i] = *gi
+			for x := i + 1; x < prevJ; x++ {
+				swapped[x] = flights[x]
+			}
+			swapped[prevJ] = *gjPrev
 			swapped[j] = *gj
-			for x := j + 1; x < len(flights); x++ {
+            for x := j + 1; x < prevK; x++ {
+                swapped[x] = flights[x]
+            }
+            swapped[prevK] = *gkPrev
+            swapped[k] = *gk
+			for x := k + 1; x < len(flights); x++ {
+				swapped[x] = flights[x]
+			}
+			comm.send(NewSolution(swapped), u.originalEngine)
+		}
+	}
+}
+func swap3b(comm comm, g Graph, u update, i, j, k int) {
+	flights := u.solution.flights
+	prevI := i - 1
+	prevJ := j - 1
+	prevK := k - 1
+	fiPrev := flights[prevI]
+	fjPrev := flights[prevJ]
+	fkPrev := flights[prevK]
+	giPrev := g.get(fiPrev.From, fiPrev.Day, fkPrev.To)
+	gjPrev := g.get(fjPrev.From, fjPrev.Day, fiPrev.To)
+	gkPrev := g.get(fkPrev.From, fkPrev.Day, fjPrev.To)
+	fi := flights[i]
+	fj := flights[j]
+	fk := flights[k]
+	gi := g.get(fk.From, fi.Day, fi.To)
+	gj := g.get(fi.From, fj.Day, fj.To)
+	gk := g.get(fj.From, fk.Day, fk.To)
+	if exists(giPrev) && exists(gjPrev) && exists(gkPrev) && exists(gi) && exists(gj) && exists(gk) {
+		oldF := []Flight{fiPrev, fi, fjPrev, fj, fkPrev, fk}
+		newF := []Flight{*giPrev, *gi, *gjPrev, *gj, *gkPrev, *gk}
+		if Cost(oldF) > Cost(newF) {
+			swapped := make([]Flight, len(flights))
+			for x := 0; x < prevI; x++ {
+				swapped[x] = flights[x]
+			}
+			swapped[prevI] = *giPrev
+			swapped[i] = *gi
+			for x := i + 1; x < prevJ; x++ {
+				swapped[x] = flights[x]
+			}
+			swapped[prevJ] = *gjPrev
+			swapped[j] = *gj
+            for x := j + 1; x < prevK; x++ {
+                swapped[x] = flights[x]
+            }
+            swapped[prevK] = *gkPrev
+            swapped[k] = *gk
+			for x := k + 1; x < len(flights); x++ {
 				swapped[x] = flights[x]
 			}
 			comm.send(NewSolution(swapped), u.originalEngine)
@@ -122,22 +181,59 @@ func swapAdj(comm comm, g Graph, u update, i, j int) {
 	}
 }
 
-func (p Polisher) run(comm comm, u update, timeout <-chan time.Time) {
+func (p Polisher) run2(comm comm, u update) {
+    start := time.Now()
 	n := len(u.solution.flights)
 	max := n - 1
 	for i := 1; i < max; i++ {
 		for j := i + 1; j <= max; j++ {
-			diff := i - j
-			if i > j {
-				i, j = j, i
-			}
-
-			if diff == 1 || diff == -1 {
-				swapAdj(comm, graph, u, i, j)
-			} else {
-				swap(comm, graph, u, i, j)
-			}
+            swap(comm, graph, u, i, j)
 		}
 	}
-	printInfo("polisher done")
+	printInfo("polisher2 done in", time.Since(start))
+}
+
+func order(i, j int) (int, int) {
+    if i < j {
+        return i, j
+    }
+    return j, i
+}
+
+func order3(i, j, k int) (int, int, int) {
+    i, j = order(i, j)
+    j, k = order(j, k)
+    i, k = order(i, k)
+    return i, j, k
+}
+
+func (p Polisher) run3(comm comm, u update) {
+    start := time.Now()
+	n := len(u.solution.flights)
+    if n < 130 {
+        maxi := n - 2
+        maxj := n - 1
+        for i := 1; i < maxi; i++ {
+            for j := i + 1; j < maxj; j++ {
+                for k := j + 1; k <= maxj; k++ {
+                    swap3a(comm, graph, u, i, j, k)
+                    swap3b(comm, graph, u, i, j, k)
+                }
+            }
+        }
+    } else {
+        timeout := time.After(3 * time.Second)
+        seed := rand.New(rand.NewSource(time.Now().UnixNano()))
+        i := seed.Intn(n-1)+1
+        j := seed.Intn(n-1)+1
+        k := seed.Intn(n-1)+1
+        i, j, k = order3(i, j, k)
+
+        for !expired(timeout) {
+            swap3a(comm, graph, u, i, j, k)
+            swap3b(comm, graph, u, i, j, k)
+        }
+    }
+
+	printInfo("polisher3 done in", time.Since(start))
 }
